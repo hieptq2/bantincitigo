e5e35d4fa900e419ccf77f20f4bab9c292604ca4
hieptq2 <hieptq11@gmail.com>
2018-12-20 18:28:45 +0700

    update

--- html/assets/js/modules/home.js
+++ html/assets/js/modules/home.js
@@ -27,6 +27,24 @@ define([], function(){
           $(this).parent('li').addClass('active');
         });
 
+        // scroll if has id tag on url
+        if($('#SiteContainer').hasClass('p-front')){
+          console.log('1');
+          var $currentURL = window.location.href; 
+          var $siteURL = $currentURL.split("#")[0];
+          var $id = $currentURL.split("#")[1];
+          console.log($currentURL);
+          console.log($siteURL);
+          console.log($id);
+          if($id){
+            scrollTo('#' + $id);
+            window.setTimeout(function() {
+              window.history.replaceState(null, null, $siteURL);
+            }, 100);
+          }
+        }
+
+
         // add class active when scroll to section
 
 
